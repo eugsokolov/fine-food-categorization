91b0a564a02ce5c1ca143d784cbe3305f8cdeecc
eugene sokolov <eugsokolov@gmail.com>
2016-04-13 12:12:30 -0400

    attempting BOW and preprocessing

--- query.py
+++ query.py
@@ -1,22 +1,37 @@
 import sqlite3
 import pandas as pd
 import numpy as np
-import nltk
+
 import string
+from nltk import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+#nltk.download('punkt')
+#nltk.download('stopwords')
 
 con = sqlite3.connect('data/database.sqlite')
 
-def bag_of_words(data):
+# https://www.kaggle.com/c/word2vec-nlp-tutorial/details/part-1-for-beginners-bag-of-words
+def preprocess(text):
+	tokens = text.str.lower().str.split()
+	tokens = word_tokenize(tokens)
+	return tokens
 
- return 0
+def bag_of_words(data):
+	bow = {}
+	tokens = preprocess(data['Text'])
+	for t in tokens:
+	   if word in bow:
+		bow[t] += 1.0
+	   else:
+		bow[t] = 1.0
+	return bow
 
 low = pd.read_sql_query("""
 SELECT Score, Summary, Text
 FROM Reviews
 WHERE Score < 3
 """, con)
-
-print(low['Summary'])
 
 med = pd.read_sql_query("""
 SELECT Score, Summary, Text
@@ -35,5 +50,5 @@ medBOW = bag_of_words(med)
 highBOW = bag_of_words(high)
 
 
-
+print highBOW
 
