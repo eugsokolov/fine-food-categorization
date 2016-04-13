f9c089a4dc58840a79128845756a9c8e2f0829b2
Frank Longueira <franklongueira@gmail.com>
2016-04-12 20:52:04 -0400

    Test

--- query.py
+++ query.py
@@ -4,25 +4,19 @@ import numpy as np
 import nltk
 import string
 
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.cross_validation import train_test_split
-from sklearn.metrics import confusion_matrix
-from sklearn import metrics
-from sklearn.metrics import roc_curve, auc
-from nltk.stem.porter import PorterStemmer
-
-con = sqlite3.connect('database.sqlite')
+con = sqlite3.connect('data/database.sqlite')
 
 def bag_of_words(data):
 
- return bow
+ return 0
 
 low = pd.read_sql_query("""
 SELECT Score, Summary, Text
 FROM Reviews
 WHERE Score < 3
 """, con)
+
+print(low['Summary'])
 
 med = pd.read_sql_query("""
 SELECT Score, Summary, Text
