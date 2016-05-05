5cbb614955edf2d305494c837fda004173a72963
eugene sokolov <eugsokolov@gmail.com>
2016-05-05 15:48:06 -0400

    working

--- modelAnalysis.py
+++ modelAnalysis.py
@@ -11,15 +11,17 @@ from nltk.corpus import stopwords
 #nltk.download('stopwords')
 
 from sklearn.cross_validation import train_test_split
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import metrics
 
 con = sqlite3.connect('data/database.sqlite')
 
 def partition(x):
 	if x < 3:
-		return 'negative'
+		return (-1)
 	else:
-		return 'positive'
+		return (1)
 
 q = pd.read_sql_query("""
 SELECT Score, Summary, Text
@@ -52,6 +54,7 @@ def tokenize(text):
 
 ### Training Set
 corpus = []
+i=0
 for text in Xtrain:
     text = text.lower()
     text = text.translate(trantab)
@@ -60,6 +63,7 @@ for text in Xtrain:
 
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(corpus)
+
 
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
@@ -80,27 +84,27 @@ prediction = dict()
 
 from sklearn import linear_model
 logreg = linear_model.LogisticRegression(C=1e5)
-logreg.fit(X_train_tfidf, y_train)
+logreg.fit(X_train_tfidf, Ytrain)
 prediction['Logistic'] = logreg.predict(X_test_tfidf)
 
 from sklearn import svm
-clf = svm.SVR().fit(X_train_tfidf, y_train)
+clf = svm.SVR().fit(X_train_tfidf, Ytrain)
 prediction['SVM'] = logreg.predict(X_test_tfidf)
 
 from sklearn.ensemble import RandomForestClassifier
-rf = RandomForestClassifier(n_estimators=10).fit(X_train_tfidf, y_train)
-prediction['Random Forest'] = rf.predict(X_test_tfidf)
+#rf = RandomForestClassifier(n_estimators=2).fit(X_train_tfidf, Ytrain)
+#prediction['Random Forest'] = rf.predict(X_test_tfidf)
 
 from sklearn.naive_bayes import MultinomialNB
-model = MultinomialNB().fit(X_train_tfidf, y_train)
+model = MultinomialNB().fit(X_train_tfidf, Ytrain)
 prediction['Multinomial'] = model.predict(X_test_tfidf)
 
 from sklearn.naive_bayes import BernoulliNB
-model = BernoulliNB().fit(X_train_tfidf, y_train)
+model = BernoulliNB().fit(X_train_tfidf, Ytrain)
 prediction['Bernoulli'] = model.predict(X_test_tfidf)
 
-y = np.array(y_test).astype('str')
-for model, predicted in predictions.iteriterms():
+y = np.array(Ytest).astype('str')
+for model, predicted in prediction.items():
  pred = np.array(predicted).astype('str')
  print(model)
  print(metrics.classification_report(y, pred))
